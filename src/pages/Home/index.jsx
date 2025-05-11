fb3810549c4a4d85fcaaa3d228ea9129da4a2271
thiagorezendev <thiagor3zende@gmail.com>
2025-05-11 17:27:41 -0300

    feat: complete project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,177 @@
+import "./style.css";
+import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
+import { useEffect, useState, useRef } from "react";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  const inputFirstName = useRef();
+  const inputLastName = useRef();
+  const inputEmail = useRef();
+  const inputPassword = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/users");
+
+    setUsers(usersFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post('/users', {
+      firstName: inputFirstName.current.value,
+      lastName: inputLastName.current.value,
+      email: inputEmail.current.value,
+      password: inputPassword.current.value
+    })
+
+    getUsers();
+  }
+
+    async function deleteUsers(id) {
+    await api.delete(`/users/${id}`)
+
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-16">
+          <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img
+              class="w-8 h-8 mr-2"
+              src="/src/assets/meeting.png"
+              alt="logo"
+            />
+            MeetUnimontes
+          </p>
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create an account
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="firstName"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Jhon"
+                    required=""
+                    ref={inputFirstName}
+                  />
+                </div>
+                <div>
+                  <label
+                    for="lastName"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Doe"
+                    required=""
+                    ref={inputLastName}
+                  />
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required=""
+                    ref={inputEmail}
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                    ref={inputPassword}
+                  />
+                </div>
+                <div className="text-right">
+                  <button
+                    onClick={createUsers}
+                    type="submit"
+                    class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Create an account
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          {/* Listar Usuários */}
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-8 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Lista de Usuários
+              </h2>
+              {users.map((user) => (
+                <div
+                  key={user.id}
+                  className="w-full bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700 p-6 mb-4"
+                >
+                  <div className="space-y-2">
+                    <p className="text-sm text-gray-900 dark:text-gray-400">
+                      <b>Nome:</b> {user.firstName} {user.lastName}
+                    </p>
+                    <p className="text-sm  text-gray-900 dark:text-gray-400">
+                      <b>Email:</b> {user.email}
+                    </p>
+                  </div>
+                  <div className="flex justify-end">
+                    <button 
+                    onClick={() => deleteUsers(user.id)}
+                    className="flex items-center px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                      <img src={Trash} alt="Delete" className="w-5 h-5 mr-2" />
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
