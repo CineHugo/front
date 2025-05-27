9193b9b6f8a021010a3689e6e4195dd45ceb0820
BrantLauro <brantlauro@gmail.com>
2025-05-26 21:39:56 -0300

    feat: add nome e logo CineHugo

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -33,8 +33,8 @@ function Register() {
       <Toaster position="top-right" />
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-16">
         <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="/src/assets/meeting.png" alt="logo" />
-          MeetUnimontes
+          <img class="w-8 h-8 mr-2" src="/src/assets/cinehugo.svg" alt="logo" />
+          CineHugo
         </p>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
